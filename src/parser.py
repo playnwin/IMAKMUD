ee7e37805adf9bc54d949b3e4ca82b853c384df7
Elijah <abney42@gmail.com>
2016-12-07 14:30:54 -0500

    Fixed a bug where players could not look at a person with a space in their name.

--- src/parser.py
+++ src/parser.py
@@ -47,6 +47,8 @@ def look(protocol, text):
     else:
         if (text[0] in world.rooms[world.players[protocol.name].location].contains.keys()) or (text[0] in world.rooms[world.players[protocol.name].location].items.keys()):
             protocol.sendMessage("You look at {}. {}".format(text[0], world.rooms[world.players[protocol.name].location].contains[text[0]].desc).encode("utf8"))
+        elif " ".join(text) in world.rooms[world.players[protocol.name].location].contains.keys():
+            protocol.sendMessage("You look at {}. {}".format(" ".join(text), world.rooms[world.players[protocol.name].location].contains[" ".join(text)].desc).encode("utf8"))
         else:
             protocol.sendMessage("You don't see {}.".format(text[0]).encode('utf8'))
 
