fd41953618e655dc233c075847bc30d96d3de052
playnwin <playnwinplayer@gmail.com>
2016-12-10 13:23:50 -0600

    Worked on rearranging character-side weapon and armor processing

--- classes/Character.py
+++ classes/Character.py
@@ -7,28 +7,11 @@ class Character:
         self.name = name
         self.desc = desc
         self.location = location
-        self.items = {}
-        self.equipped = {'head': None, 'neck': None, 'chest': None, 'back': None, 'hands': None,
-                         'legs': None, 'feet': None, 'ring1': None, 'ring2': None, 'misc1': None, 'misc2': None,
-                         'misc3': None, 'mainHand': None, 'offHand': None}
-        self.unmodifiedstats = {'dr' : 0, 'hp' : 100, 'accuracy' : 20}
-        self.stats = {'dr' : 0, 'hp' : 100, 'accuracy' : 20}
+        self.stats = {"health": 100, "accuracy": 0, "damage": 50, "weapon_type": "piercing",
+                      "damage_reduction": 10, "armor_type": "chain"}
 
     def is_player(self):
         return False
-
-    def change_room(self, new_room):
-        world.rooms[self.location].remove_entity(self)
-        world.rooms[self.location].alert_exit(self.name)
-        world.rooms[new_room].add_entity(self)
-        world.rooms[new_room].alert_entrance(self.name)
-        self.location = new_room
-
-    def add_item(self, item):
-        self.items[item.id] = item
-
-    def say(self, text):
-        world.rooms[self.location].broadcast(self.name, text)
 
     def update_stats(self):
         self.stats = dict(self.unmodifiedstats)
@@ -126,14 +109,66 @@ class Character:
                     self.equipped['mainHand'] = None
                     self.equipped['offHand'] = None
                     self.update_stats()
+"""
+class Player(Character):
+    def __init__(self, name="", desc="", password="", location=""):
+        Character.__init__(self, name, desc, location)
+        self.password = password
+        self.items = {}
+        self.equipped = {'head': None, 'neck': None, 'chest': None, 'back': None, 'hands': None,
+                         'legs': None, 'feet': None, 'ring1': None, 'ring2': None, 'misc1': None, 'misc2': None,
+                         'misc3': None, 'mainHand': None, 'offHand': None}
+        self.unmodifiedstats = {'dr' : 0, 'hp' : 100, 'accuracy' : 20}
+        self.stats = {'dr' : 0, 'hp' : 100, 'accuracy' : 20}
+
+    def is_player(self):
+        return True
+
+    def change_room(self, new_room):
+        world.rooms[self.location].remove_entity(self)
+        world.rooms[self.location].alert_exit(self.name)
+        world.rooms[new_room].add_entity(self)
+        world.rooms[new_room].alert_entrance(self.name)
+        self.location = new_room
+
+    def add_item(self, item):
+        self.items[item.id] = item
+
+    def say(self, text):
+        world.rooms[self.location].broadcast(self.name, text)
+
+    def think(self, text):
+        print("Telling {}, {}".format(self.name, text))
+        for k,v in world.factory.clients.items():
+            if self.name == v:
+                world.factory.link[k].sendMessage(text.encode("utf8"))
+"""
 
 class Player(Character):
     def __init__(self, name="", desc="", password="", location=""):
         Character.__init__(self, name, desc, location)
         self.password = password
+        self.items = {}
+        self.equipped = {'head': None, 'neck': None, 'chest': None, 'back': None, 'hands': None,
+                         'legs': None, 'feet': None, 'ring1': None, 'ring2': None, 'misc1': None, 'misc2': None,
+                         'misc3': None, 'mainHand': None, 'offHand': None}
+        self.stats = {"health": 100, "mana": 100, "strength": 10, "agility": 10, "spellcasting: 10"}
 
     def is_player(self):
         return True
+
+    def change_room(self, new_room):
+        world.rooms[self.location].remove_entity(self)
+        world.rooms[self.location].alert_exit(self.name)
+        world.rooms[new_room].add_entity(self)
+        world.rooms[new_room].alert_entrance(self.name)
+        self.location = new_room
+
+    def add_item(self, item):
+        self.items[item.id] = item
+
+    def say(self, text):
+        world.rooms[self.location].broadcast(self.name, text)
 
     def think(self, text):
         print("Telling {}, {}".format(self.name, text))
